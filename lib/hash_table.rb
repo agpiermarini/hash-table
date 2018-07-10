096faaebb1916d44cb7e695251a5f65f70735538
agpiermarini <piermarini.andrew@gmail.com>
2018-07-09 18:56:14 -0600

    Add HashTable#CalculateIndex

--- lib/hash_table.rb
+++ lib/hash_table.rb
@@ -1,11 +1,18 @@
 require './lib/linked_list'
 
 class HashTable
-  attr_reader :size
   attr_accessor :array
 
   def initialize(size = 10)
     @size = size
     @array = Array.new(size, LinkedList.new)
   end
+
+  def calculate_index(key)
+    key.sum % size
+  end
+
+  private
+    attr_reader :size
+
 end
--- test/hash_table_test.rb
+++ test/hash_table_test.rb
@@ -4,8 +4,25 @@ require './lib/hash_table'
 
 class HashTableTest < Minitest::Test
   def test_it_exists
-    table = HashTable.new()
+    table = HashTable.new
 
     assert table.class, HashTable
+  end
+
+  def test_it_can_initializes_with_array_of_empty_linked_lists
+    table_array = HashTable.new.array
+
+    assert table_array.length, 10
+    assert table_array.all? { |e| e.class == LinkedList }
+  end
+
+  def test_it_can_calculate_index_based_on_key
+    table = HashTable.new
+    assert table.calculate_index("Andrew"), 9
+    assert table.calculate_index("Cobra Kai"), 6
+  end
+
+  def test_it_can_add_values_to_table
+    table = HashTable.new
   end
 end
